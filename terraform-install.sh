17cb97b6923daf9f4f4026ee2b7fe855ed75c1a4
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2021-06-18 10:24:34 -0400

    Update terraform-install.sh

--- terraform-install.sh
+++ terraform-install.sh
@@ -7,7 +7,7 @@ Export terraform binary path temporally
 export PATH=$PATH:/usr/local/bin
 #Add path permanently for current user.By Exporting path in .bashrc file at end of file.
 vi .bashrc
- echo  "export PATH='$PATH:/usr/local/bin'" >> ~/.bashrc
+ echo  "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
 # Source .bashrc to reflect for current session
  source ~/.bashrc 
  terraform -version
